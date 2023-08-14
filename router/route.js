d5f2e51b987cc738d68562fa36024afe943de0b0
shubham paul <91714143+Shu12388y@users.noreply.github.com>
2023-08-14 08:03:57 +0530

    Update route.js

--- router/route.js
+++ router/route.js
@@ -1,13 +1,24 @@
 const path = require("path")
 const fs = require("fs")
+const { MovieDetails, getMoviebyId } = require("../controller/movieController")
+
+
 
 function route(req,res){
-    if(req.url==="/"){
+    if(req.url==="/" && req.method==="GET"){
         const homePage=fs.readFile(path.join(__dirname,"../views","index.html"),"utf-8",(err,content)=>{
         res.end(content)
     })
+}
+else if (req.url==="/api/getmovies" && req.method==="GET"){
+   MovieDetails(req,res)
 
-    }
+}
+else if (req.url.match(/\/movie\/([0-9]+)/) && req.method === "GET"){
+    let id=req.url.match(/\/movie\/([0-9]+)/)[1];
+    getMoviebyId(req,res,id)
+
+}
 }
 
 
